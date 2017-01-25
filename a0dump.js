89770ac7de31187cd591d4fb4278a77ba673efce
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2017-01-25 12:49:58 +0100

    Filtered global apps

--- a0dump.js
+++ a0dump.js
@@ -123,7 +123,7 @@ authenticate(config)
 	var getConnections = getjson('connections')
 		.then(writeConnections());
 	var getClients = getjson('clients')
-		.then((clients) => clients.filter((c) => c.name != "All Applications"))
+		.then((clients) => clients.filter((c) => c.global != true))
 		.then((clients) => clients.filter((c) => c.client_id != config.AUTH0_CLIENT_ID))
 		.then(writeFiles('clients'));
 	var getRules = getjson('rules')
